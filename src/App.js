db7115dc05bf0381e322bed231b76a16b9615708
hgutierrez <hgutierrez@OSC0100239>
2021-12-27 16:03:04 -0400

    Delete Function

--- src/App.js
+++ src/App.js
@@ -16,15 +16,35 @@ function addTask(name) {
   setTasks([...tasks, newTask]);
   }
 
+  function toggleTaskCompleted(id) {
+    const updatedTasks = tasks.map(task =>{
+      if (id === task.id){
+        return {...task, completed: !task.completed}
+      }
+      return task;
+  });
+   setTasks(updatedTasks);
+  }
+
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+  setTasks(remainingTasks);
+  }
+
+
   const taskList = tasks.map(task => (
     <Todo
         id={task.id}
         name={task.name}
         completed={task.completed}
         key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
       />
     )
   );
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
@@ -34,7 +54,7 @@ function addTask(name) {
         <FilterButton name="Active"/>
         <FilterButton name= "Completed"/>
       </div>
-      <h2 id="list-heading">3 tasks remaining</h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
        //role="list"
         className="todo-list stack-large stack-exception"
