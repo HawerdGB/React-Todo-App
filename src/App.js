8c49ecd0a953b6222ff01881b168c1bdb611fa68
hgutierrez <hgutierrez@OSC0100239>
2021-12-28 09:20:10 -0400

    Editing and Filtering

--- src/App.js
+++ src/App.js
@@ -5,10 +5,18 @@ import Todo from "./components/Todo";
 import { nanoid } from "nanoid";
 
 
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 
 function App(props) {
 
 const [tasks, setTasks] = useState(props.tasks);
+const [filter, setFilter] = useState('All');
   
 
 function addTask(name) {
@@ -31,18 +39,38 @@ function addTask(name) {
   setTasks(remainingTasks);
   }
 
+  function editTask(id, newName){
+    const editedTaskList = tasks.map(task => {
+      if(id === task.id){
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editedTaskList);
+  }
 
-  const taskList = tasks.map(task => (
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map(task => (
     <Todo
-        id={task.id}
-        name={task.name}
-        completed={task.completed}
-        key={task.id}
-        toggleTaskCompleted={toggleTaskCompleted}
-        deleteTask={deleteTask}
-      />
-    )
-  );
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
+  
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
   const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
   const headingText = `${taskList.length} ${tasksNoun} remaining`;
   return (
@@ -50,9 +78,8 @@ function addTask(name) {
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton name="All" />
-        <FilterButton name="Active"/>
-        <FilterButton name= "Completed"/>
+      {filterList}
+
       </div>
       <h2 id="list-heading">{headingText}</h2>
       <ul
